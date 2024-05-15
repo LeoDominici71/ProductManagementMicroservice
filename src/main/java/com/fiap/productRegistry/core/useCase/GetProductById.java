f3e1f61d184f814aab60227cdc38dcc71a5c3ad4
LeoDominici71 <leonardo_requena@outlook.com>
2024-05-15 12:17:55 -0300

    final commit

--- src/main/java/com/fiap/productRegistry/core/useCase/GetProductById.java
+++ src/main/java/com/fiap/productRegistry/core/useCase/GetProductById.java
@@ -3,6 +3,8 @@ package com.fiap.productRegistry.core.useCase;
 import com.fiap.productRegistry.core.ProductRepository;
 import com.fiap.productRegistry.core.entities.Products;
 
+import jakarta.persistence.EntityNotFoundException;
+
 public class GetProductById {
 
 	private ProductRepository productRepository;
@@ -12,7 +14,11 @@ public class GetProductById {
 	}
 	
 	public Products getProductById(Long id) {
-		return productRepository.getProductById(id);
+				Products product = productRepository.getProductById(id);
+				if(product.getStock().equals("0")) {
+					throw new EntityNotFoundException("There is no product on stock");
+				}
+				return product;
 	}
 
 }
