cbdd9e8466de62d2b95f9174ca6b9ee1dff60372
LeoDominici71 <leonardo_requena@outlook.com>
2024-05-19 21:27:35 -0300

    final commit

--- src/main/java/com/fiap/productRegistry/ProductRegistryApplication.java
+++ src/main/java/com/fiap/productRegistry/ProductRegistryApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 @EnableDiscoveryClient
 @SpringBootApplication
-@LoadBalancerClient(name = "productManagement")
 public class ProductRegistryApplication {
 
 	public static void main(String[] args) {
